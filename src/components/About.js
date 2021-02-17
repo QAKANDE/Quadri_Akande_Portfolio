f70009fd25cf36400b9f80b5ae92a45250cd6d67
QOA <58850207+QAKANDE@users.noreply.github.com>
2021-02-17 17:12:49 +0000

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import { Icon } from "@iconify/react";
+import mongo from "@iconify/icons-logos/mongodb";
+import reactIcon from "@iconify/icons-logos/react";
+import express from "@iconify/icons-logos/express";
+import node from "@iconify/icons-logos/nodejs";
+
+class About extends Component {
+    render() {
+        if (this.props.sharedBasicInfo) {
+            var profilepic = "images/" + this.props.sharedBasicInfo.image;
+        }
+        if (this.props.resumeBasicInfo) {
+            var sectionName = this.props.resumeBasicInfo.section_name.about;
+            var hello = this.props.resumeBasicInfo.description_header;
+            var about = this.props.resumeBasicInfo.description;
+        }
+
+        return ( <
+            section id = "about" >
+            <
+            div className = "col-md-12" >
+            <
+            h1 style = {
+                { color: "black" }
+            } >
+            <
+            span > { sectionName } < /span> < /
+            h1 > <
+            div className = "row center mx-auto mb-5" >
+            <
+            div className = "col-md-4 mb-5 center" >
+            <
+            div className = "polaroid" >
+            <
+            span style = {
+                { cursor: "auto" }
+            } >
+            <
+            img height = "250px"
+            src = { profilepic }
+            alt = "Avatar placeholder" /
+            >
+            <
+            Icon icon = { mongo }
+            style = {
+                { fontSize: "200%", margin: "9% 5% 0 5%" }
+            }
+            /> <
+            Icon icon = { express }
+            style = {
+                { fontSize: "100%", margin: "9% 5% 0 5%" }
+            }
+            /> <
+            Icon icon = { reactIcon }
+            style = {
+                { fontSize: "200%", margin: "9% 5% 0 5%" }
+            }
+            /> <
+            Icon icon = { node }
+            style = {
+                { fontSize: "200%", margin: "9% 5% 0 5%" }
+            }
+            /> < /
+            span > <
+            /div> < /
+            div >
+
+            <
+            div className = "col-md-8 center" >
+            <
+            div className = "col-md-10" >
+            <
+            div className = "card" >
+            <
+            div className = "card-header" >
+            <
+            span className = "iconify"
+            dataicon = "emojione:red-circle"
+            datainline = "false" >
+            <
+            /span>{" "} &
+            nbsp; { " " } <
+            span className = "iconify"
+            dataicon = "twemoji:yellow-circle"
+            datainline = "false" >
+            <
+            /span>{" "} &
+            nbsp; { " " } <
+            span className = "iconify"
+            dataicon = "twemoji:green-circle"
+            datainline = "false" >
+            <
+            /span> < /
+            div > <
+            div className = "card-body font-trebuchet text-justify ml-3 mr-3"
+            style = {
+                {
+                    height: "auto",
+                    fontSize: "132%",
+                    lineHeight: "200%",
+                }
+            } >
+            <
+            br / >
+            <
+            span className = "wave" > { hello }: ) < /span> <
+        br / >
+            <
+            br / > { about } <
+            /div> < /
+        div > <
+            /div> < /
+        div > <
+            /div> < /
+        div > <
+            /section>
+    );
+}
+}
+
+export default About;
